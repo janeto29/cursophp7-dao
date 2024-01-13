c6d1e291dc03ca536334f94ba3b473c6ac2909c7
Jose Lustosa <janeto29@gmail.com>
2024-01-12 21:35:31 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 
 require_once("config.php");
 
+//carrega um usuario 
+//$usuario = new Usuario();
+
+//$usuario->loadById(7);
+
+//$usuario->delete();
+
+//echo $usuario;
+//$search = Usuario::search("a");
+
+//echo json_decode($search);
+
 $usuario = new Usuario();
-
-$usuario->loadById(7);
-
-$usuario->delete();
+$usuario->login("root", "!@#$%%");
 
 echo $usuario;
  ?>
